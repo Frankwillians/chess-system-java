82167964ab53d1e6b275dfb1203614427482a8bd
Frankwillians <fkffrannk02@gmail.com>
2024-04-02 14:43:52 -0300

    First class: Position

--- src/application/progam.java
+++ src/application/progam.java
@@ -1,5 +1,10 @@
 package application;
 
-public class progam {
+import boardgame.Position;
 
+public class progam {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
 }
